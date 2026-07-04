6f069c69a67b9336329cd3f680a220e7a6e52456
Adlo029 <johonskripka@gmail.com>
2026-07-04 16:33:16 +0500

    abdulloh

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_ACCESS_TOKEN || 'SUPER_SECRET_KEY',
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'postgres',
+      autoLoadModels: true,
+      synchronize: true,
+      // sync: { force: true },
+    }),
+  ],
 })
 export class AppModule {}
